22e5533defa60f7158f7610e17e21b75a8a7e096
CrisB93 <clbg93@hotmail.com>
2021-12-13 11:56:47 -0500

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Finally could learn this <code>src/App.js</code> and save to get fucking rich!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I will get a fucking great job!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
